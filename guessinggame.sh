7546c7e993e7bad08619894af5d8b958c26bc23f
javadebadi <javad.ebadi.1990@gmail.com>
2017-12-02 21:51:52 +0330

    added guessinggame.sh and makefile

---
+++ guessinggame.sh
@@ -0,0 +1,30 @@
+# This program will continuously ask the user to guess the number of files in the current directory, until they guess the correct number.
+
+echo "How many files are in the current directory? Guess the number."
+
+read guess
+
+number_of_files_in_directory=$(ls |wc -w)
+
+
+# function howBig takes two argument and assumes that the first number is the true number and the second number is your guess
+function howBig {
+if [[ $2 -gt $1 ]] 
+then echo "Your guess is too big"
+elif [[ $1 -gt $2 ]] 
+then echo "Your guess is too low"
+elif [[ $2 -eq $1 ]]
+then echo "Congratulation! Your guess was correct."
+fi
+}
+
+#a loop functin for the user the guess the number until it is correct
+while [[ $guess -ne $number_of_files_in_directory ]]
+do
+howBig $number_of_files_in_directory $guess
+echo "Try agian: "
+read guess
+done
+
+
+howBig $number_of_files_in_directory $guess
---
+++ makefile
@@ -0,0 +1,4 @@
+README.md: guessinggame.sh
+	echo "# Peer-graded Assignment: Guessing Game" > README.md
+	date >> README.md
+	wc -l guessinggame.sh >> README.md
